62ffc8e26f535c63fe3ec05bd77f561a8f07e0a5
Jnan <vjnan369@gmail.com>
2018-09-03 15:06:54 +0530

    added comments

--- src/com/example/BookStore.java
+++ src/com/example/BookStore.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -12,10 +13,12 @@ public class BookStore implements Store{
         this.storeId = storeId;
     }
 
+    // will add a new book to the store
     public void add(Book book) {
         books.add(book);
     }
 
+    // To get the book details of given bookid
     public Book getBookDetails(String bookId) {
         for(Book book : books) {
             if (book.getBookId().equals(bookId)) {
@@ -25,18 +28,23 @@ public class BookStore implements Store{
         throw new NoSuchElementException("Unable to find given book");
     }
 
+    // returns the total number of books available in store
     public int getBooksCount(){
         return books.size();
     }
 
+    // returns store id of invoked object
     public String getStoreId() {
         return storeId;
     }
 
+    // returns List of all books available in store.
     public List<Book> getAllBooks() {
-        return books;
+        return Collections.unmodifiableList(books);
     }
 
+    // removes the purchased book from store
+    // if the book is not available to purchase, it will throw an error.
     public void purchase(String bookId) {
         Book purchaseBook = null;
         for(Book book : books) {
